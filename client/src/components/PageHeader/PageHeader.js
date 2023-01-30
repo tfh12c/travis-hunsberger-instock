03a866d8fe007ed289754c84ffc95094530900af
Travis Hunsberger <tfhunsberger@gmail.com>
2023-01-30 16:46:16 -0500

    header jsx template finished. starting styling

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,9 +1,25 @@
 import './PageHeader.scss';
+import logo from '../../assets/logos/InStock-Logo.png';
+import { Link } from 'react-router-dom';
 
 function PageHeader() {
 
     return (
-        <div>PageHeader</div>
+        <header className="header">
+            <button className='header__instock-button'>
+                <img className='header__instock-image' src={logo} alt="instock logo"></img>
+            </button>
+            <nav className='header__nav'>
+                <ul className='header__nav_list'>
+                    <li className='header__nav-item'>
+                        <Link className='header__nav-warehouse-link' to="/warehouse">Warehouses</Link>
+                    </li>
+                    <li className='header__nav-item'>
+                        <Link className='header__nav-inventory-link' to="/inventory">Inventory</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
