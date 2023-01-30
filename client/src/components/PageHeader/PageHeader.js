e81d3d2843ec14bd406b6ea72a6fcf22994d9c58
Travis Hunsberger <tfhunsberger@gmail.com>
2023-01-30 18:48:11 -0500

    PageHeader styling complete for mobile

--- client/src/components/PageHeader/PageHeader.js
+++ client/src/components/PageHeader/PageHeader.js
@@ -1,21 +1,21 @@
 import './PageHeader.scss';
-import logo from '../../assets/logos/InStock-Logo.png';
-import { Link } from 'react-router-dom';
+import logo from '../../assets/logos/InStock-Logo.svg';
+import { Link, NavLink } from 'react-router-dom';
 
 function PageHeader() {
 
     return (
         <header className="header">
-            <button className='header__instock-button'>
+            <Link className='header__instock-link' to="/">
                 <img className='header__instock-image' src={logo} alt="instock logo"></img>
-            </button>
+            </Link>
             <nav className='header__nav'>
-                <ul className='header__nav_list'>
+                <ul className='header__nav-list'>
                     <li className='header__nav-item'>
-                        <Link className='header__nav-warehouse-link' to="/warehouse">Warehouses</Link>
+                        <NavLink className='header__nav-link' to="/warehouse">Warehouses</NavLink>
                     </li>
                     <li className='header__nav-item'>
-                        <Link className='header__nav-inventory-link' to="/inventory">Inventory</Link>
+                        <NavLink className='header__nav-link' to="/inventory">Inventory</NavLink>
                     </li>
                 </ul>
             </nav>
