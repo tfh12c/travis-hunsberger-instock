040542a80bcb9520b5df0b5a6f7735b36b606c3d
Travis Hunsberger <tfhunsberger@gmail.com>
2023-02-12 19:51:10 -0500

    updated index.scss page to have global style for header tags, removed from mixins the associated h1 and h2 header/subheader mixins created. this fixed a styling issue where font size for header on add new warehouse page was too large

--- client/src/pages/AddWarehousePage/AddWarehousePage.js
+++ client/src/pages/AddWarehousePage/AddWarehousePage.js
@@ -1,9 +1,22 @@
 import './AddWarehousePage.scss';
+import arrowBack from '../../assets/icons/arrow_back.svg';
 
 function AddWarehousePage() {
 
     return (
-        <div>AddWarehousePage</div>
+        <main className='add-warehouse-page'>
+            <section className='add-warehouse-page__content-container'>
+                <div className='add-warehouse-page__header-content'>
+                    <button className='add-warehouse-page__arrow-back-button'>
+                        <img className='add-warehouse-page__arrow-back-icon' src={arrowBack} alt='arrow back icon'></img>
+                    </button>
+                    <h2 className='add-warehouse-page__header'>Add New Warehouse</h2>
+                </div>
+                {/* {error && <p>{error}</p>}
+                {loading && <p>Loading...</p>}
+                {data && <addWarehouseForm warehouse={data} id={id} />} */}
+            </section>
+        </main>
     )
 }
 
