d022b85d84ad8a215b926985078e581baf1105cb
Travis Hunsberger <tfhunsberger@gmail.com>
2023-03-15 16:39:10 -0400

    delete warehouse modal styled for tablet view. added X button to close the modal

--- client/src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
+++ client/src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
@@ -1,10 +1,14 @@
 import './DeleteWarehouseModal.scss'
+import close from '../../assets/icons/close.svg'
 
 function DeleteWarehouseModal({ closeDeleteModal, handleDelete, warehouse }) {
 
     return (
         <div className='delete-warehouse-modal'>
             <div className='delete-warehouse-modal__modal-content'>
+                <button className='delete-warehouse-modal__close-button' onClick={() => closeDeleteModal()}>
+                    <img className='delete-warehouse-modal__close' src={close} alt='close icon'></img>
+                </button>
                 <div className='delete-warehouse-modal__text-container'>     
                     <h1 className='delete-warehouse-modal__header'>Delete {warehouse.name} warehouse?</h1>
                     <p className='delete-warehouse-modal__body'>Please confirm that you'd like to delete {warehouse.name} from the list of warehouses. You won't be able to undo this action.</p>
