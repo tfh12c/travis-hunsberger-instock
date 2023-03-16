47a3c0663e1de9f9835268264f74856be3ea9674
Travis Hunsberger <tfhunsberger@gmail.com>
2023-03-16 17:45:49 -0400

    edit warehouse page styled for tablet

--- client/src/components/EditWarehouseForm/EditWarehouseForm.js
+++ client/src/components/EditWarehouseForm/EditWarehouseForm.js
@@ -56,43 +56,45 @@ function EditWarehouseForm({ warehouse, id }) {
     return (
         <>
             <form className='edit-warehouse-form' onSubmit={handleSubmit}>
-                <div className='edit-warehouse-form__warehouse-details-container'>
-                    <h2 className='edit-warehouse-form__header'>Warehouse Details</h2>    
-                    {/* Warehouse Name Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='name'>Warehouse Name</label>
-                    <input className='edit-warehouse-form__input' type='text' name='name' id='name' onChange={handleChange} value={formValues.name}></input>
-                    {!formValues.name && formError}
-                    {/* Warehouse Address Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='address'>Street Address</label>
-                    <input className='edit-warehouse-form__input' type='text' name='address' id='address' onChange={handleChange} value={formValues.address}></input>
-                    {!formValues.address && formError}
-                    {/* Warehouse City Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='city'>City</label>
-                    <input className='edit-warehouse-form__input' type='text' name='city' id='city' onChange={handleChange} value={formValues.city}></input>
-                    {!formValues.city && formError}
-                    {/* Warehouse Country Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='country'>Country</label>
-                    <input className='edit-warehouse-form__input' type='text' name='country' id='country' onChange={handleChange} value={formValues.country}></input>
-                    {!formValues.country && formError}
-                </div>
-                <div className='edit-warehouse-form__contact-details-container'>
-                    <h2 className='edit-warehouse-form__header'>Contact Details</h2>       
-                    {/* Contact Name Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='contactName'>Contact Name</label>
-                    <input className='edit-warehouse-form__input' type='text' name='contactName' id='contactName' onChange={handleChange} value={formValues.contactName}></input>
-                    {!formValues.contactName && formError}
-                    {/* Contact Position Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='position'>Position</label>
-                    <input className='edit-warehouse-form__input' type='text' name='position' id='position' onChange={handleChange} value={formValues.position}></input>
-                    {!formValues.position && formError}
-                    {/* Contact Phone Number Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='phone'>Phone Number</label>
-                    <input className='edit-warehouse-form__input' type='tel' name='phone' id='phone' onChange={handleChange} value={formValues.phone}></input>
-                    {!formValues.phone && formError}
-                    {/* Contact Email Label/Input */}
-                    <label className='edit-warehouse-form__label' htmlFor='email'>Email</label>
-                    <input className='edit-warehouse-form__input' type='email' name='email' id='email' onChange={handleChange} value={formValues.email}></input>
-                    {!formValues.email && formError}
+                <div className='edit-warehouse-form__input-section'>
+                    <div className='edit-warehouse-form__warehouse-details-container'>
+                        <h2 className='edit-warehouse-form__header'>Warehouse Details</h2>    
+                        {/* Warehouse Name Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='name'>Warehouse Name</label>
+                        <input className='edit-warehouse-form__input' type='text' name='name' id='name' onChange={handleChange} value={formValues.name}></input>
+                        {!formValues.name && formError}
+                        {/* Warehouse Address Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='address'>Street Address</label>
+                        <input className='edit-warehouse-form__input' type='text' name='address' id='address' onChange={handleChange} value={formValues.address}></input>
+                        {!formValues.address && formError}
+                        {/* Warehouse City Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='city'>City</label>
+                        <input className='edit-warehouse-form__input' type='text' name='city' id='city' onChange={handleChange} value={formValues.city}></input>
+                        {!formValues.city && formError}
+                        {/* Warehouse Country Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='country'>Country</label>
+                        <input className='edit-warehouse-form__input' type='text' name='country' id='country' onChange={handleChange} value={formValues.country}></input>
+                        {!formValues.country && formError}
+                    </div>
+                    <div className='edit-warehouse-form__contact-details-container'>
+                        <h2 className='edit-warehouse-form__header'>Contact Details</h2>       
+                        {/* Contact Name Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='contactName'>Contact Name</label>
+                        <input className='edit-warehouse-form__input' type='text' name='contactName' id='contactName' onChange={handleChange} value={formValues.contactName}></input>
+                        {!formValues.contactName && formError}
+                        {/* Contact Position Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='position'>Position</label>
+                        <input className='edit-warehouse-form__input' type='text' name='position' id='position' onChange={handleChange} value={formValues.position}></input>
+                        {!formValues.position && formError}
+                        {/* Contact Phone Number Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='phone'>Phone Number</label>
+                        <input className='edit-warehouse-form__input' type='tel' name='phone' id='phone' onChange={handleChange} value={formValues.phone}></input>
+                        {!formValues.phone && formError}
+                        {/* Contact Email Label/Input */}
+                        <label className='edit-warehouse-form__label' htmlFor='email'>Email</label>
+                        <input className='edit-warehouse-form__input' type='email' name='email' id='email' onChange={handleChange} value={formValues.email}></input>
+                        {!formValues.email && formError}
+                    </div>
                 </div>
                 <div className='edit-warehouse-form__button-container'>
                     <button className='edit-warehouse-form__cancel-button' type='button' onClick={handleCancel}>Cancel</button>
