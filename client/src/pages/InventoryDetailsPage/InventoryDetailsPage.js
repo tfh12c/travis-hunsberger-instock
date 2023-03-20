be4a3d446e1c5480f190c817d80d513b1e9e2488
Travis Hunsberger <tfhunsberger@gmail.com>
2023-03-20 11:17:11 -0400

    inventory details page styled for tablet

--- client/src/pages/InventoryDetailsPage/InventoryDetailsPage.js
+++ client/src/pages/InventoryDetailsPage/InventoryDetailsPage.js
@@ -31,6 +31,8 @@ function InventoryDetailsPage() {
         getInventoryById(id);
     }, [id])
 
+    console.log(data);
+
     return (
         <main className='inventory-details-page'>
             {error && <p>{error}</p>}
@@ -48,28 +50,35 @@ function InventoryDetailsPage() {
                     <Link to={`/inventory/edit/${data.id}`}>
                         <button className='inventory-details-page__edit-button'>
                             <img className='inventory-details-page__edit-icon' src={edit} alt='edit icon'></img>
+                            <p className='inventory-details-page__edit-text'>Edit</p>
                         </button>
                     </Link>
                 </div>
                 <div className='inventory-details-page__item-details'>
-                    <h4 className='inventory-details-page__item-header'>ITEM DESCRIPTION:</h4>
-                    <p className='inventory-details-page__description'>{data.description}</p>
-                    <h4 className='inventory-details-page__item-header'>CATEGORY:</h4>
-                    <p className='inventory-details-page__category'>{data.category}</p>
-                    <div className='inventory-details-page__status-quantity-container'>
-                        <div className='inventory-details-page__status-content'>
-                            <h4 className='inventory-details-page__item-header'>STATUS:</h4>
-                            <p className='inventory-details-page__status'>
-                            <span className={data.quantity ? 'mobile-inventory-card__status-tag--instock' : 'mobile-inventory-card__status-tag--outofstock'}>{data.status}</span>
-                            </p>
+                    <div className='inventory-details-page__item-category-container'>
+                        <h4 className='inventory-details-page__item-header'>ITEM DESCRIPTION:</h4>
+                        <p className='inventory-details-page__description'>{data.description}</p>
+                        <h4 className='inventory-details-page__item-header'>CATEGORY:</h4>
+                        <p className='inventory-details-page__category'>{data.category}</p>
+                    </div>
+                    <div className='inventory-details-page__status-quantity-warehouse-container'>
+                        <div className='inventory-details-page__status-quantity-container'>
+                            <div className='inventory-details-page__status-content'>
+                                <h4 className='inventory-details-page__item-header'>STATUS:</h4>
+                                <p className='inventory-details-page__status'>
+                                    <span className={data.quantity ? 'inventory-details-page__status-tag--instock' : 'inventory-details-page__status-tag--outofstock'}>{data.status}</span>
+                                </p>
+                            </div>
+                            <div className='inventory-details-page__quantity-content'>
+                                <h4 className='inventory-details-page__item-header'>QUANTITY:</h4>
+                                <p className='inventory-details-page__quantity'>{data.quantity}</p>
+                            </div>
                         </div>
-                        <div className='inventory-details-page__quantity-content'>
-                            <h4 className='inventory-details-page__item-header'>QUANTITY:</h4>
-                            <p className='inventory-details-page__quantity'>{data.quantity}</p>
+                        <div className='inventory-details-page__warehouse-content'>
+                            <h4 className='inventory-details-page__item-header'>WAREHOUSE:</h4>
+                            <p className='inventory-details-page__warehouse'>{data.warehouseName}</p>
                         </div>
                     </div>
-                    <h4 className='inventory-details-page__item-header'>WAREHOUSE:</h4>
-                    <p className='inventory-details-page__warehouse'>{data.warehouseName}</p>
                 </div>
             </section>}
         </main>
