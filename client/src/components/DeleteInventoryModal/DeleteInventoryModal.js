92156dae78b538506c380178cb17cec92a32455a
Travis Hunsberger <tfhunsberger@gmail.com>
2023-03-20 10:26:20 -0400

    delete inventory modal styled for tablet

--- client/src/components/DeleteInventoryModal/DeleteInventoryModal.js
+++ client/src/components/DeleteInventoryModal/DeleteInventoryModal.js
@@ -1,10 +1,14 @@
 import './DeleteInventoryModal.scss';
+import close from '../../assets/icons/close.svg';
 
 function DeleteInventoryModal({ closeDeleteModal, handleDelete, item }) {
 
     return (
         <div className='delete-inventory-modal'>
             <div className='delete-inventory-modal__modal-content'>
+                <button className='delete-inventory-modal__close-button' onClick={() => closeDeleteModal()}>
+                    <img className='delete-inventory-modal__close' src={close} alt='close icon'></img>
+                </button>
                 <div className='delete-inventory-modal__text-container'>     
                     <h1 className='delete-inventory-modal__header'>Delete {item.itemName} inventory item?</h1>
                     <p className='delete-inventory-modal__body'>Please confirm that you'd like to delete {item.itemName} from the inventory list. You won't be able to undo this action.</p>
