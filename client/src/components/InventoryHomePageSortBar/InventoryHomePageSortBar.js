2bbf1d7c822d283b5350be546c3fa4e125c048e3
Travis Hunsberger <tfhunsberger@gmail.com>
2023-03-21 14:53:26 -0400

    updated sort bars for desktop styling

--- client/src/components/InventoryHomePageSortBar/InventoryHomePageSortBar.js
+++ client/src/components/InventoryHomePageSortBar/InventoryHomePageSortBar.js
@@ -6,23 +6,23 @@ function InventoryHomePageSortBar({ sort, sortInventory, sortCategory, sortStatu
     return (
         <div className='inventory-home-page-sort-bar__sort-buttons-container'>
             <button className='inventory-home-page-sort-bar__sort' onClick={sortInventory}>
-                <h4 className='inventory-home-page-sort-bar__warehouse-sort-header'>INV. ITEM</h4>
+                <h4 className='inventory-home-page-sort-bar__inventory-sort-header'>INV. ITEM</h4>
                 <img className={sort === 'inventory' ? 'inventory-home-page-sort-bar__sort-icon--active' : ''} src={sortIcon} alt='sort icon'></img>
             </button>
             <button className='inventory-home-page-sort-bar__sort' onClick={sortCategory}>
-                <h4 className='inventory-home-page-sort-bar__address-sort-header'>CATEGORY</h4>
+                <h4 className='inventory-home-page-sort-bar__category-sort-header'>CATEGORY</h4>
                 <img className={sort === 'category' ? 'inventory-home-page-sort-bar__sort-icon--active' : ''} src={sortIcon} alt='sort icon'></img>
             </button>
             <button className='inventory-home-page-sort-bar__sort' onClick={sortStatus}>
-                <h4 className='inventory-home-page-sort-bar__contact-name-sort-header'>STATUS</h4>
+                <h4 className='inventory-home-page-sort-bar__status-sort-header'>STATUS</h4>
                 <img className={sort === 'status' ? 'inventory-home-page-sort-bar__sort-icon--active' : ''} src={sortIcon} alt='sort icon'></img>
             </button>
             <button className='inventory-home-page-sort-bar__sort' onClick={sortQuantity}>
-                <h4 className='inventory-home-page-sort-bar__contact-info-sort-header'>QTY</h4>
+                <h4 className='inventory-home-page-sort-bar__quantity-sort-header'>QTY</h4>
                 <img className={sort === 'quantity' ? 'inventory-home-page-sort-bar__sort-icon--active' : ''} src={sortIcon} alt='sort icon'></img>
             </button>
             <button className='inventory-home-page-sort-bar__sort' onClick={sortWarehouse}>
-                <h4 className='inventory-home-page-sort-bar__contact-info-sort-header'>WAREHOUSE</h4>
+                <h4 className='inventory-home-page-sort-bar__warehouse-sort-header'>WAREHOUSE</h4>
                 <img className={sort === 'warehouse' ? 'inventory-home-page-sort-bar__sort-icon--active' : ''} src={sortIcon} alt='sort icon'></img>
             </button>
             <button className='inventory-home-page-sort-bar__actions'>
